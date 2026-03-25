cf1fc36f0b042504d57c28ad98cb1a6407df0481
nikhil kandhare <98297220+1900690105@users.noreply.github.com>
2026-03-25 16:29:32 +0530

    small changes

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -130,7 +130,6 @@ export default function Projects() {
             Current Top Projects
           </span>
         </h2>
-
         <div className="grid md:grid-cols-2 gap-8">
           {projects.map((project, index) => (
             <div
